670142775dcf82d8ca8a3eb496e9b4c85c0f4911
tdubs42 <contact-tdubs@protonmail.com>
2021-08-11 17:41:07 -0700

    added secret

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const dotenv = require('dotenv')
   .config()
 const express = require('express')
 const cors = require('cors')
-const app = express()
+
 const port = process.env.PORT || 9000
+const secret = process.env.SECRET
+
+const app = express()
 
 app.use(cors())
 app.use(express.json())
 
 app.use("/api/*",(_,res)=>{
-  res.json({data: "The API is working?"})
+  res.json({data: `${secret} says hello`})
 })
 
 app.listen(port, () => {

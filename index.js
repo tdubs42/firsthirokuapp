113197e57c6dbc7ff117335881fc3ed0216d9cc0
tdubs42 <contact-tdubs@protonmail.com>
2021-08-11 17:35:06 -0700

    deployment push

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-console.log("It's alive")
+const dotenv = require('dotenv')
+  .config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 9000
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/*",(_,res)=>{
+  res.json({data: "The API is working?"})
+})
+
+app.listen(port, () => {
+  console.log(`Serving running on port ${port}`)
+})
+
+console.log('It\'s alive')
 console.log(__dirname)
 console.log(__filename)
 console.log(process.env.USER)
+console.log(process.env.PORT)

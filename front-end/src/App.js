07b7266ee3d6ddabc22ff22deb2e15bcdd8c274e
tdubs42 <contact-tdubs@protonmail.com>
2021-08-11 18:06:53 -0700

    front-end push

---
+++ front-end/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <section className="App">
+      <header className="App-header">
+        <h1>tdubs 1st REST API</h1>
+      </header>
+    </section>
+  );
+}
+
+export default App;
